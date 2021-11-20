c81bb3e26fd3f849db5d1a43ffc2b78f8f786209
iamsaravan97 <saransaravana1997@gmail.com>
2021-11-20 11:01:43 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -2,34 +2,46 @@ import './style.css';
 
 import { of, map, Observable } from 'rxjs';
 
-const anotherObservable$ = new Observable<string>((sub) => {
-  sub.next('kowsi');
-  sub.next('naks');
-  sub.next('sathya');
-});
 
-const Observable$ = new Observable<string>((sub) => {
-  sub.next('sarvana');
-  sub.next('sarala');
-  sub.next('sandy');
-  setTimeout(() => {
-    sub.next('keerthi');
-  }, 4000);
-  if (!sub.closed) sub.next('arumugam');
+ourownof<string>('saro','sachin','dhoni','kamal').subscribe(val=>console.log(val));
+
+function ourownof<T>(...args :T[]):Observable<T>{
+return new Observable(sub=>{
+  for(let i=0;i<args.length;i++){
+    sub.next(args[i]);
+  }
   sub.complete();
-  return () => {
-    console.log('subscibe ends');
-  };
-});
+})
+}
 
-const observer = {
-  next: (val) => console.log(val),
-  error: (error) => console.log(error),
-  complete: () => console.log('completed successfully'),
-};
+// const anotherObservable$ = new Observable<string>((sub) => {
+//   sub.next('kowsi');
+//   sub.next('naks');
+//   sub.next('sathya');
+// });
 
-const subscription = Observable$.subscribe(observer);
-console.log(' before unsubscribe');
-subscription.unsubscribe();
-console.log(' after unsubscribe');
+// const Observable$ = new Observable<string>((sub) => {
+//   sub.next('sarvana');
+//   sub.next('sarala');
+//   sub.next('sandy');
+//   setTimeout(() => {
+//     sub.next('keerthi');
+//   }, 4000);
+//   if (!sub.closed) sub.next('arumugam');
+//   sub.complete();
+//   return () => {
+//     console.log('subscibe ends');
+//   };
+// });
+
+// const observer = {
+//   next: (val) => console.log(val),
+//   error: (error) => console.log(error),
+//   complete: () => console.log('completed successfully'),
+// };
+
+// const subscription = Observable$.subscribe(observer);
+// console.log(' before unsubscribe');
+// subscription.unsubscribe();
+// console.log(' after unsubscribe');
 // Open the console in the bottom right to see results.
